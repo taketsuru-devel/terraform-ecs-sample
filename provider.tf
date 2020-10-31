43b42fc6257a48f3b340a7eae879abd612b2be73
followedwind <followedwind@gmail.com>
2020-10-31 09:28:23 +0000

    tfstateをs3に

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-tfstate-s3"
+    region = "ap-northeast-1"
+    key = "ecs-sample"
+  }
+}
+
 provider "aws" {
   region = "ap-northeast-1"
   profile = "default"
